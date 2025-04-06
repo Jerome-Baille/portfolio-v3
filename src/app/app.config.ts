dffc080a77a254ffbebca46a7f154e8f2f09408b
Jerome-Baille <baille.jerome@gmail.com>
2025-04-06 18:42:36 +0200

    Add new icons and update web manifest for portfolio-v3
    - Added icon images: icon-72x72.png and icon-96x96.png.
    - Created a web manifest file with app details and icon specifications.
    - Included multiple icon sizes for better compatibility across devices.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { IMAGE_LOADER } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { customImageLoader } from './about-carousel/about-carousel.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     {
       provide: IMAGE_LOADER,
       useValue: customImageLoader
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
